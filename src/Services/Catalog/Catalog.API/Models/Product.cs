2f30496afcff7cc2852114ae397aebcd4a32270f
lukada-taiya <lukmanadam43@gmail.com>
2025-01-26 16:37:13 +0000

    Feat: Modified Product model

--- src/Services/Catalog/Catalog.API/Models/Product.cs
+++ src/Services/Catalog/Catalog.API/Models/Product.cs
@@ -2,7 +2,7 @@ namespace Catalog.API.Models;
 
 public class Product
 {
-    public Guid Guid { get; set; }
+    public Guid Id { get; set; }
     public string Name { get; set; } = default!;
     public List<string> Category { get; set; } = new ();
     public string Description { get; set; } = default!;
