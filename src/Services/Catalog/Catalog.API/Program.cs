8a966bc1453f2ec54f31fa736cb0e821e483695b
lukada-taiya <lukmanadam43@gmail.com>
2025-01-26 16:35:25 +0000

    Feat: Install Marten library to use with postgres

--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -6,6 +6,20 @@ builder.Services.AddMediatR(config =>
 {
     config.RegisterServicesFromAssembly(typeof(Program).Assembly);
 }); 
+
+builder.Services.AddMarten(opt =>
+{ 
+    try
+    {
+        opt.Connection(builder.Configuration.GetConnectionString("Database")!);
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"Error configuring Marten: {ex.Message}");
+        throw;
+    }
+}).UseLightweightSessions();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline. 
