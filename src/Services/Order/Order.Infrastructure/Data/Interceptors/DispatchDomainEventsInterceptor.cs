b7932c114bd73b3875bfd29655543f1010486df7
lukada-taiya <lukmanadam43@gmail.com>
2025-09-15 13:55:54 +0000

    Feat: Implemented configurations and interceptors for order models

---
+++ src/Services/Order/Order.Infrastructure/Data/Interceptors/DispatchDomainEventsInterceptor.cs
@@ -0,0 +1,35 @@
+namespace Order.Infrastructure.Data.Interceptors;
+
+public class DispatchDomainEventsInterceptor(IMediator mediator) : SaveChangesInterceptor
+{
+    public override InterceptionResult<int> SavingChanges(DbContextEventData eventData, InterceptionResult<int> result)
+    {
+        DispatchDomainEvents(eventData.Context).GetAwaiter().GetResult();
+        return base.SavingChanges(eventData, result);
+    }
+
+    public override async ValueTask<InterceptionResult<int>> SavingChangesAsync(DbContextEventData eventData, InterceptionResult<int> result, CancellationToken cancellationToken = new CancellationToken())
+    {
+        await DispatchDomainEvents(eventData.Context);
+        return await base.SavingChangesAsync(eventData, result,cancellationToken);
+    }
+
+    private async Task DispatchDomainEvents(DbContext? context)
+    {
+        if (context != null)
+        {
+            var aggregates = context.ChangeTracker.Entries<IAggregate>()
+                .Where(e => e.Entity.DomainEvents.Any())
+                .Select(e => e.Entity);
+            
+            var domainEvents = aggregates.SelectMany(e => e.DomainEvents).ToList();
+            
+            aggregates.ToList().ForEach(e => e.ClearDomainEvents());
+
+            foreach (var domainEvent in domainEvents)
+            {
+                await mediator.Publish(domainEvent);
+            }
+        }
+    }
+}
