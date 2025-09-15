ee286570528e080ef3cbd677dc459bcb224c7568
lukada-taiya <lukmanadam43@gmail.com>
2025-09-15 13:49:55 +0000

    Enhancement: Modifications to entity models

--- src/Services/Order/Order.Domain/Abstractions/Entity.cs
+++ src/Services/Order/Order.Domain/Abstractions/Entity.cs
@@ -4,7 +4,7 @@ public abstract class Entity<T> : IEntity<T>
 {
     public DateTime? CreatedAt { get; set; }
     public string? CreatedBy { get; set; }
-    public DateTime LastModified { get; set; }
-    public string LastModifiedBy { get; set; }
+    public DateTime? LastModified { get; set; }
+    public string? LastModifiedBy { get; set; }
     public T Id { get; set; }
 }
--- src/Services/Order/Order.Domain/Abstractions/IEntity.cs
+++ src/Services/Order/Order.Domain/Abstractions/IEntity.cs
@@ -8,6 +8,6 @@ public interface IEntity
 {
     public DateTime? CreatedAt { get; set; }
     public string? CreatedBy { get; set; }
-    public DateTime LastModified { get; set; }
-    public string LastModifiedBy { get; set; }
+    public DateTime? LastModified { get; set; }
+    public string? LastModifiedBy { get; set; }
 }
