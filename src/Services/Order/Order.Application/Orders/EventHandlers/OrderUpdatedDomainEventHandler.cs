e39b900ff805b999eb4a2761140165e3fb34ec2e
lukada-taiya <lukmanadam43@gmail.com>
2025-09-15 14:04:17 +0000

    Feat: Added domain event handlers for Orders

---
+++ src/Services/Order/Order.Application/Orders/EventHandlers/OrderUpdatedDomainEventHandler.cs
@@ -0,0 +1,13 @@
+using Microsoft.Extensions.Logging;
+using Order.Domain.Events;
+
+namespace Order.Application.Orders.EventHandlers;
+
+public class OrderUpdatedDomainEventHandler(ILogger<OrderUpdatedDomainEventHandler> logger) : INotificationHandler<OrderUpdatedEvent>
+{
+    public async Task Handle(OrderUpdatedEvent notification, CancellationToken cancellationToken)
+    {
+        logger.LogInformation("Event: Order Has Been Updated. {Time}", notification.ToString());
+        await Task.CompletedTask;
+    }
+}
