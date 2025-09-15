62f7e5cd2fc7d43e9131367110435b0e71701b0b
lukada-taiya <lukmanadam43@gmail.com>
2025-09-15 14:05:09 +0000

    Feat: Implemented command and command handler to update orders

---
+++ src/Services/Order/Order.Application/Orders/Commands/UpdateOrder/UpdateOrderCommand.cs
@@ -0,0 +1,15 @@
+namespace Order.Application.Orders.Commands.UpdateOrder;
+
+public record UpdateOrderCommand(OrderDto Order) : ICommand<UpdateOrderResult>;
+
+public record UpdateOrderResult(bool IsSuccess);
+
+public class UpdateOrderCommandValidator : AbstractValidator<UpdateOrderCommand>
+{
+    public UpdateOrderCommandValidator()
+    {
+        RuleFor(x => x.Order.Id).NotEmpty().WithMessage("Order Id is required.");
+        RuleFor(x => x.Order.OrderName).NotEmpty().WithMessage("Order Name is required.");
+        RuleFor(x => x.Order.CustomerId).NotEmpty().WithMessage("Customer Id is required.");
+    }
+}
