eadc3a81902f616eda5069296a2e27539e9586bc
lukada-taiya <lukmanadam43@gmail.com>
2025-09-17 21:10:16 +0000

    Feat: Added feature management functionality

--- src/Services/Order/Order.Application/DependencyInjection.cs
+++ src/Services/Order/Order.Application/DependencyInjection.cs
@@ -3,12 +3,13 @@ using BuildingBlocks.Behaviours;
 using BuildingBlocks.Messaging.MassTransit;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.FeatureManagement;
 
 namespace Order.Application;
 
 public static class DependencyInjection
 {
-    public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+    public static IServiceCollection AddApplicationServices(this IServiceCollection services, IConfiguration configuration)
     {
         services.AddMediatR(opt =>
         {
@@ -16,6 +17,8 @@ public static class DependencyInjection
             opt.AddOpenBehavior(typeof(LoggingBehaviour<,>));
             opt.AddOpenBehavior(typeof(ValidationBehaviour<,>));
         });
+
+        services.AddFeatureManagement();
         services.AddMessageBroker(configuration, Assembly.GetExecutingAssembly());
         
         return services;
