457b92c0341f68ee0fbc69d4750c6a96f124186e
lukada-taiya <lukmanadam43@gmail.com>
2025-09-20 07:24:08 +0000

    Feat: Implemented api gateway for microservice

---
+++ src/ApiGateways/YarpApiGateway/Program.cs
@@ -0,0 +1,17 @@
+using Microsoft.AspNetCore.RateLimiting;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddReverseProxy().LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
+builder.Services.AddRateLimiter(options =>
+{
+    options.AddFixedWindowLimiter("fixed", opt =>
+    {
+        opt.PermitLimit = 5;
+        opt.Window = TimeSpan.FromSeconds(10);
+    });
+});
+var app = builder.Build();
+app.UseRateLimiter();
+app.MapReverseProxy();
+app.Run();
